e34011413727a38a3e08d57a889cd8e9b4b85f6c
littlefire <aybwhite@gmail.com>
2021-02-11 21:58:28 -0800

    added files within folders- added connection to server.js ((new to review connecting to mongodb

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//Connecting to public
+app.use(express.static("public"));
+
+//Connecting to route files
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+//Connecting to mongodb
+//mongoose.connect("mongodb://localhost/workout", {
+//    useNewUrlParser: true,
+//    useFindAndModify: false
+//});
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}!`);
+});
