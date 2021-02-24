6d7ff430d38e8e3c36cdd92cee4a0fe26b4acf5c
littlefire <aybwhite@gmail.com>
2021-02-23 18:08:46 -0800

    finishing get and put in api.js

--- routes/api.js
+++ routes/api.js
@@ -8,15 +8,36 @@ router.post("/api/workouts", (req,res) => {
         res.json(err);
     });
 });
-router.put("/api/workouts:id", () => {
 
+router.put("/api/workouts:id", ({body, params}, res) => {
+    Workout.findByIdAndUpdate(
+        params.id, {$push:{exercises:body}},
+        {new:true, runValidators: true}
+    ).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    })
 });
+
 router.get("/api/workouts", (req,res) => {
-
+    Workout.find().then(dbWorkouts => {
+        res.json(dbWorkouts);
+    }).catch(err => {
+        res.json(err);
+    });
 });
+
 router.get("/api/workouts/range", (req,res) => {
-
+    Workout.find({}).limit(5)
+    .then(dbWorkouts => {
+        console.log(dbWorkouts)
+        res.json(dbWorkouts);
+    }).catch(err => {
+        res.json(err);
+    });
 });
+
 router.delete("/api/workouts", (req,res) => {
     Workout.findByIdAndDelete(body.id)
     .then(() => {
