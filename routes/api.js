19f6f08f2b160e5f81b1efca5eecd1a729fb7c7b
littlefire <aybwhite@gmail.com>
2021-02-23 17:41:53 -0800

    router post and delete

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", (req,res) => {
-
+    Workout.create({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
 });
 router.put("/api/workouts:id", () => {
 
@@ -14,7 +18,12 @@ router.get("/api/workouts/range", (req,res) => {
 
 });
 router.delete("/api/workouts", (req,res) => {
-
+    Workout.findByIdAndDelete(body.id)
+    .then(() => {
+        res.json(true);
+    }).catch(err => {
+        res.json(err);
+    });
 });
 
 module.exports = router;
