322b25209060e0bfaa0f9934e4d4ff6f83acef07
littlefire <aybwhite@gmail.com>
2021-02-23 17:34:18 -0800

    base line router:post/put/get/delete skeletons

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req,res) => {
+
+});
+router.put("/api/workouts:id", () => {
+
+});
+router.get("/api/workouts", (req,res) => {
+
+});
+router.get("/api/workouts/range", (req,res) => {
+
+});
+router.delete("/api/workouts", (req,res) => {
+
+});
+
+module.exports = router;
