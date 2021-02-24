a34376b73bd19e435c8bdf092324d70de707b77c
littlefire <aybwhite@gmail.com>
2021-02-23 18:24:19 -0800

    fixed api route name in put

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ router.post("/api/workouts", (req,res) => {
     });
 });
 
-router.put("/api/workouts:id", ({body, params}, res) => {
+router.put("/api/workouts/:id", ({body, params}, res) => {
     Workout.findByIdAndUpdate(
         params.id, {$push:{exercises:body}},
         {new:true, runValidators: true}
@@ -17,7 +17,7 @@ router.put("/api/workouts:id", ({body, params}, res) => {
         res.json(dbWorkout);
     }).catch(err => {
         res.json(err);
-    })
+    });
 });
 
 router.get("/api/workouts", (req,res) => {
